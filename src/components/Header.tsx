b532131ecbdc5b5db915aba98c3965d563aa7cd3
Cauan Victor <91569130+CauanDZN@users.noreply.github.com>
2022-03-20 13:54:18 -0300

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ export function Header() {
   return (
     <header className="header">
       <div>
-        <img src="/todo.png" alt="to.do"/>
+        <h1>to.do</h1>
       </div>
     </header>
   )
